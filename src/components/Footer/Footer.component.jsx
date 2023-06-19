f0a18b394627385b27bcb86431d0d99212d71cf8
Joshua Cavell <xclusive36@gmail.com>
2023-06-19 14:35:57 -0400

    update footer component with content

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -1,3 +1,18 @@
-const Footer = () => <div>Footer component!</div>;
+import Social from "../Social/Social.component";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Social />
+      <p className="footer-text">
+        Built with <a href="https://react.dev/">React</a> and{" "}
+        <a href="https://ionicframework.com/">Ionic Framework</a>
+      </p>
+      <p className="footer-text">
+        &copy; {new Date().getFullYear()} <a href="#contact">Joshua Cavell</a>
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
